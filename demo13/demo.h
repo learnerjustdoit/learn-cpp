0451a76ee6f399584703539d95ec7a9effc7f58a
learnerjustdoit <1109885671@qq.com>
2020-12-28 12:41:19 +0800

    add demo.h

---
+++ demo13/demo.h
@@ -0,0 +1,16 @@
+#ifndef _DEMO_H
+#define _DEMO_H
+
+//头文件一般写如下内容：
+//类的定义
+//外部变量的声明
+//函数的声明
+
+//只能写声明，不能写定义，类的定义以及const常量的定义除外
+// double a;不可以
+// extern double a;可以
+// extern double a=0.0;不可以
+
+//const常量的定义可以写在头文件里
+const int b = 99;
+#endif // !_DEMO_H
